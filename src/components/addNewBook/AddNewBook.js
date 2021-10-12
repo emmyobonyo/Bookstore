23a975b72e4fe3e42709df2474d1eb62d4156a99
emmyobonyo@gmail.com <emmyobonyo@gmail.com>
2021-10-12 15:09:12 +0300

    Add form to create new book

--- src/components/addNewBook/AddNewBook.js
+++ src/components/addNewBook/AddNewBook.js
@@ -1,11 +1,22 @@
-import React from 'react'
+import React from 'react';
+import Select from 'react-select';
+
+const options = [
+  { value: 'Fiction', label: 'Fiction' },
+  { value: 'Non-fiction', label: 'Non-fiction' },
+  { value: 'Self-help', label: 'Self-help' },
+];
 
 function AddNewBook() {
   return (
     <div>
-      <h2>Add a New Book</h2>
+      <h3>ADD NEW BOOK</h3>
+      <form>
+        <input type="text" placeholder="Book Title" />
+        <Select options={options} />
+      </form>
     </div>
-  )
+  );
 }
 
-export default AddNewBook
+export default AddNewBook;
--- src/components/homePage/Home.js
+++ src/components/homePage/Home.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import AddNewBook from '../addNewBook/AddNewBook';
 
 function Home() {
   return (
     <div>
       <h3>Home</h3>
+      <AddNewBook />
     </div>
   );
 }
