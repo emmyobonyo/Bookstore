d3de9286548ffb5b23a00c7beb76ff923b441b49
emmyobonyo@gmail.com <emmyobonyo@gmail.com>
2021-10-14 23:16:56 +0300

    Add an Async function to collect books from the API

--- src/components/addNewBook/AddNewBook.js
+++ src/components/addNewBook/AddNewBook.js
@@ -1,41 +1,28 @@
 import React from 'react';
-import { useDispatch } from 'react-redux';
-import { v4 as uuidv4 } from 'uuid';
-import { addBook, removeBook } from '../../redux/books/books';
+// import { useDispatch } from 'react-redux';
+// import { v4 as uuidv4 } from 'uuid';
+// import { addBook, removeBook } from '../../redux/books/books';
 
 function AddNewBook() {
-  const dispatch = useDispatch();
-  const submitBookToStore = (e) => {
-    e.preventDefault();
-    const newBook = {
-      id: uuidv4(),
-      title: document.getElementById('bookTitle').value,
-      author: document.getElementById('author').value,
-    };
-
-    dispatch(addBook(newBook));
-
-    const bookList = document.getElementById('list-books');
-    const div = document.createElement('div');
-    const title = document.createElement('h2');
-    title.innerHTML = newBook.title;
-    const author = document.createElement('p');
-    author.innerHTML = newBook.author;
-    const button = document.createElement('button');
-    button.innerHTML = 'Remove';
-    button.type = 'button';
-    button.onclick = () => {
-      dispatch(removeBook(newBook));
-      bookList.removeChild(div);
-    };
-    bookList.appendChild(div);
-    div.appendChild(title);
-    div.appendChild(author);
-    div.appendChild(button);
-
-    document.getElementById('bookTitle').value = '';
-    document.getElementById('author').value = '';
+  // const dispatch = useDispatch();
+  const bookList = document.getElementById('list-books');
+  const getBook = () => {
+    fetch('https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/BhqzrQ20oG4ih4qjaX67/books')
+      .then((res) => res.json())
+      .then((data) => {
+        Object.keys(data).forEach((book) => {
+          const div = document.createElement('div');
+          const title = document.createElement('h2');
+          title.innerHTML = data[book][0].title;
+          const category = document.createElement('p');
+          category.innerHTML = data[book][0].category;
+          bookList.appendChild(div);
+          div.appendChild(title);
+          div.appendChild(category);
+        });
+      });
   };
+  getBook();
 
   return (
     <div>
@@ -44,7 +31,7 @@ function AddNewBook() {
       <form>
         <input id="bookTitle" type="text" placeholder="Book Title" />
         <input id="author" type="text" placeholder="Author" />
-        <button type="submit" onClick={submitBookToStore}>Add Book</button>
+        <button type="submit" onClick={getBook}>Add Book</button>
       </form>
     </div>
   );
