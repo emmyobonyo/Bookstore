56b38f0572bb7d6176090b6a244bb9767bc54224
emmyobonyo@gmail.com <emmyobonyo@gmail.com>
2021-10-16 00:58:44 +0300

    Add Add Book method

--- src/components/addNewBook/AddNewBook.js
+++ src/components/addNewBook/AddNewBook.js
@@ -1,39 +1,13 @@
 import React, { useEffect } from 'react';
-// import { createAsyncThunk } from '@reduxjs/toolkit';
 import { useDispatch } from 'react-redux';
-import { v4 as uuidv4 } from 'uuid';
-// import { getBooks } from '../../redux/books/books';
 import { getBooks } from '../../redux/books/getBooksSlice';
+import postBook from '../../redux/books/postBookSlice';
 
 function AddNewBook() {
   const dispatch = useDispatch();
-
   useEffect(() => {
     dispatch(getBooks());
   }, []);
-
-  const postBook = () => {
-    const id = uuidv4();
-    const title = document.getElementById('bookTitle').value;
-    const category = document.getElementById('category').value;
-    fetch('https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/BhqzrQ20oG4ih4qjaX67/books/', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        Accept: 'application/json',
-      },
-      body: JSON.stringify({ item_id: id, title, category }),
-    })
-      .then((res) => res);
-    document.getElementById('bookTitle').value = '';
-    document.getElementById('category').value = '';
-    // dispatch(addBook({
-    //   item_id: id,
-    //   title,
-    //   category,
-    // }));
-  };
-  // getBook();
 
   return (
     <div>
