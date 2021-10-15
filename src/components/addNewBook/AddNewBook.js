eae870387e0f42d40d225e79fddf047e4911f3ca
emmyobonyo@gmail.com <emmyobonyo@gmail.com>
2021-10-16 02:07:36 +0300

    Fix faulty add button

--- src/components/addNewBook/AddNewBook.js
+++ src/components/addNewBook/AddNewBook.js
@@ -1,13 +1,54 @@
 import React, { useEffect } from 'react';
+import { v4 as uuidv4 } from 'uuid';
 import { useDispatch } from 'react-redux';
 import { getBooks } from '../../redux/books/getBooksSlice';
-import postBook from '../../redux/books/postBookSlice';
 
 function AddNewBook() {
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(getBooks());
   }, []);
+
+  const postBook = () => {
+    const id = uuidv4();
+    const title = document.getElementById('bookTitle').value;
+    const category = document.getElementById('category').value;
+    fetch('https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/BhqzrQ20oG4ih4qjaX67/books/', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+      },
+      body: JSON.stringify({ item_id: id, title, category }),
+    })
+      .then((res) => res);
+    const bookList = document.getElementById('book-list');
+    const div = document.createElement('div');
+    const newTitle = document.createElement('h2');
+    newTitle.innerHTML = document.getElementById('bookTitle').value;
+    const newCategory = document.createElement('p');
+    newCategory.innerHTML = document.getElementById('category').value;
+    const button = document.createElement('button');
+    button.innerHTML = 'Remove';
+    button.type = 'button';
+    button.onclick = async function () {
+      await fetch(`https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/BhqzrQ20oG4ih4qjaX67/books/${id}`, {
+        method: 'DELETE',
+        headers: {
+          'Content-Type': 'application/json',
+          Accept: 'application/json',
+        },
+        body: JSON.stringify({ item_id: id }),
+      });
+      bookList.removeChild(div);
+    };
+    bookList.appendChild(div);
+    div.appendChild(newTitle);
+    div.appendChild(newCategory);
+    div.appendChild(button);
+    document.getElementById('bookTitle').value = '';
+    document.getElementById('category').value = '';
+  };
 
   return (
     <div>
